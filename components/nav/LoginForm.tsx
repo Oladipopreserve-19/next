13f59e0291f3971f654f74e0d8f2ed795f8d0ceb
Oladipopreserve-19 <98063668+Oladipopreserve-19@users.noreply.github.com>
2024-04-19 14:25:31 +0100

    ;

--- components/nav/LoginForm.tsx
+++ components/nav/LoginForm.tsx
@@ -6,28 +6,28 @@ import { usePathname } from "next/navigation";
 import React from "react";
 
 export default function LoginForm() {
-	const pathname = usePathname();
-	const supabase = createBrowserClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-	);
+  const pathname = usePathname();
+  const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
 
-	const handleLogin = () => {
-		supabase.auth.signInWithOAuth({
-			provider: "github",
-			options: {
-				redirectTo: `${location.origin}/auth/callback?next=${pathname}`,
-			},
-		});
-	};
+  const handleLogin = () => {
+    supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: `${location.origin}/auth/callback?next=${pathname}`,
+      },
+    });
+  };
 
-	return (
-		<Button
-			className="flex items-center gap-2"
-			variant="outline"
-			onClick={handleLogin}
-		>
-			<GitHubLogoIcon /> Login
-		</Button>
-	);
+  return (
+    <Button
+      className="flex items-center gap-2"
+      variant="outline"
+      onClick={handleLogin}
+    >
+      <GitHubLogoIcon /> Login
+    </Button>
+  );
 }
